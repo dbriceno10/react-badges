1f75cff19532c25972285f2d9cc74d1db2e685d5
nombre <daniel.b100726@gmail.com>
2021-05-14 17:48:30 -0400

    clase 6 ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
+// const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// // ReactDOM.render(__qué__, __dónde__);
+// ReactDOM.render(element, container);
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const element = <h1>Hola mundo utilizando React</h1>//ersto es gracias a JSX, para poder hacer esto así siempre hay que importar react y react-dom
+const container = document.getElementById("app")
+
+ReactDOM.render(element, container)
