d5be96cc118dc958fedf162c3fd6baccfe27a2ce
nombre <daniel.b100726@gmail.com>
2021-05-17 18:03:28 -0400

    Clase 13 nuestra primera página

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Badge from "./componets/badge"
+import BadgeNew from "./pages/badgeNew"
 import "bootstrap/dist/css/bootstrap.css"
 import "./global.css"
 
 const container = document.getElementById("app")
 
-ReactDOM.render(<Badge 
+ReactDOM.render(<BadgeNew 
     firstName="Alejandro" 
     lastName="Salazar" 
     jobTitle="Frontend Engineer in Training"       
     twitter="dbriceno10" 
     avatarUrl = "https://media-exp1.licdn.com/dms/image/C5603AQFILCh7FRKErA/profile-displayphoto-shrink_200_200/0/1617145732921?e=1626307200&v=beta&t=lkVAqzsfDRPW5nmUDPnXEVA38bpJYTI6R8KKdqWlvxs" />, container)
+
+// ReactDOM.render(<Badge 
+//     firstName="Alejandro" 
+//     lastName="Salazar" 
+//     jobTitle="Frontend Engineer in Training"       
+//     twitter="dbriceno10" 
+//     avatarUrl = "https://media-exp1.licdn.com/dms/image/C5603AQFILCh7FRKErA/profile-displayphoto-shrink_200_200/0/1617145732921?e=1626307200&v=beta&t=lkVAqzsfDRPW5nmUDPnXEVA38bpJYTI6R8KKdqWlvxs" />, container)
 //A ReactDOM Siempre hay que darle un elemento, por lo tantolo que vamos a renderizar lo vamos a pasar entre brakets como una etiqueta autoconclusiva para asegurarnos
 
 
---
+++ src/pages/badgeNew.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Navbar from "../componets/navbar"
+import header from "../images/badge-header.svg"
+import "./styles/BadgeNew.css"
+import Badge from "../componets/badge"
+
+class BadgeNew extends React.Component {
+    render() {
+        return(
+            <div>
+                <Navbar/>
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo"/>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge
+                                firstName="Alejandro" 
+                                lastName="Salazar" 
+                                jobTitle="Frontend Engineer in Training"       
+                                twitter="dbriceno10" 
+                                avatarUrl = "https://media-exp1.licdn.com/dms/image/C5603AQFILCh7FRKErA/profile-displayphoto-shrink_200_200/0/1617145732921?e=1626307200&v=beta&t=lkVAqzsfDRPW5nmUDPnXEVA38bpJYTI6R8KKdqWlvxs" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BadgeNew
