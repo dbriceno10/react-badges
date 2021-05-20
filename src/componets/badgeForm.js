eb57dbe67c8249855a47f11a3bc42551b506c493
nombre <daniel.b100726@gmail.com>
2021-05-19 22:46:46 -0400

    clase 16 levantaiento del estado

--- src/componets/badgeForm.js
+++ src/componets/badgeForm.js
@@ -6,23 +6,24 @@ class BadgeForm extends React.Component {
     //La inicialización se puede hacer en cualquier línea dentro de nuestro componente, pero lo, por buenas prácticas, es hacerlo al inicio
     // state = {} // creamos es estado simplemente pasandole un objeto vacío
     //ahora vamos a inicializar con un arreglo pero de campos "vacíos"
-    state = {//este se puede inicializar con campos vacíos o algún campo colocado por defecto el cual el usuaro va a poder cambiar cuando escriba en los inputs
-        firstName: "",
-        lastName: "",
-        email: "",
-        jobTitle: "",
-        twitter: ""
-    }
-    handleChange = (e) =>{
-        // console.log({e}) //Recordar que cuando se trabaja con react, muchas cosas se van a manejar como si de objetos se tratasen
-        // console.log({
-        //     name: e.target.name,
-        //     value: e.target.value})
-        this.setState({//función de la clase component llamada setState, nos sirve paraguardar información
-            // firstName: e.target.value, // en lugar de guardar todo en first name, vamos a pasar una llave, una variable para guardar la 
-            [e.target.name]: e.target.value,
-        })
-    }
+    // state = {//este se puede inicializar con campos vacíos o algún campo colocado por defecto el cual el usuaro va a poder cambiar cuando escriba en los 
+        // firstName: "",
+        // lastName: "",
+        // email: "",
+        // jobTitle: "",
+        // twitter: ""
+    // }
+    // handleChange = (e) =>{
+    //     // console.log({e}) //Recordar que cuando se trabaja con react, muchas cosas se van a manejar como si de objetos se tratasen
+    //     // console.log({
+    //     //     name: e.target.name,
+    //     //     value: e.target.value})
+    //     this.setState({//función de la clase component llamada setState, nos sirve paraguardar información
+    //         // firstName: e.target.value, // en lugar de guardar todo en first name, vamos a pasar una llave, una variable para guardar la 
+    //         [e.target.name]: e.target.value,
+    //     })
+    // }
+    //handleChange, el cuál es el método que maneja la información del evento lo vamos a eliminar de acá, puesto que ahora esto se va a manejar desde la página, es decir, BadgeNew
     handleClick = (e) =>{
         e.preventDefault()
         console.log("El formulario fue enviado")
@@ -40,50 +41,50 @@ class BadgeForm extends React.Component {
                     <div>
                         <label>Frist Name</label>
                         <input 
-                            onChange={this.handleChange}  
+                            onChange={this.props.onChange}  
                             className="form-control" 
                             type="text" 
                             name="firstName"
-                            value={this.state.firstName}
+                            value={this.props.formValues.firstName}
                         />
                         {/*Eventos con react, en un imput cuando escribimos, obtenemos un evento onChange, la vamos a manejar con un método de la clase que se debe crear, suele llamarse handleChange */}
                     </div>
                     <div>
                         <label>Last Name</label>
                         <input 
-                            onChange={this.handleChange}  
+                            onChange={this.props.onChange}  
                             className="form-control" 
                             type="text" 
                             name="lastName"
-                            value={this.state.lastName}
+                            value={this.props.formValues.lastName}
                         />
                     </div>
                     <div>
                         <label>Email</label>
                         <input 
-                            onChange={this.handleChange}  className="form-control" 
+                            onChange={this.props.onChange}  className="form-control" 
                             type="email" 
                             name="email"
-                            value={this.state.email}
+                            value={this.props.formValues.email}
                         />
                     </div>
                     <div>
                         <label>Job Title</label>
                         <input 
-                            onChange={this.handleChange}  
+                            onChange={this.props.onChange}  
                             className="form-control" 
                             type="text" 
                             name="jobTitle"
-                            value={this.state.jobTitle}
+                            value={this.props.formValues.jobTitle}
                         />
                         <div>
                         <label>Twitter</label>
                         <input 
-                            onChange={this.handleChange}  
+                            onChange={this.props.onChange}  
                             className="form-control" 
                             type="text" 
                             name="twitter"
-                            value={this.state.twitter}
+                            value={this.props.formValues.twitter}
                         />
                     </div>
                     </div>
--- src/pages/badgeNew.js
+++ src/pages/badgeNew.js
@@ -6,6 +6,33 @@ import Badge from "../componets/badge"
 import BadgeForm from "../componets/badgeForm"
 
 class BadgeNew extends React.Component {
+    state = { form: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        jobTitle: "",
+        twitter: ""
+    } }//inicializamos un estado vación, con una propiedad form también vacía
+    // handleChange = e => {
+    //     const nextForm = this.state.form
+    //     nextForm[e.target.name] = e.target.value
+    //     this.setState({
+    //         //form: {//form va a recibir y guardar la infomación del evento que acaba de ocurrir, esta información ahora le va a pertenecer a BadgeNew
+    //             //[e.target.name]: e.target.value// el problema aquí es que cada vez que se introduzca una nueva información, va a sobre escribir la anterior, existen dos maneras de solucionarlo, la primera es hacer una copia del estado del form
+
+    //         //}
+    //         form: nextForm,// esta es la primera forma
+    //     })
+    // }
+    handleChange = e => {//segunda manera, vamos a "dejar caer" todos los valores que tenia el form anteriormente, y añadimos otro o sobre escribimos el que ya estaba guardado, usando los puntos suspensivos "..." que indica propagación de la propiedad (prop)
+        this.setState({
+            form: {
+                ...this.state.form,
+            [e.target.name]: e.target.value,
+            }
+        })
+    }
+    
     render() {
         return(
             <div>
@@ -18,15 +45,21 @@ class BadgeNew extends React.Component {
                     <div className="row">
                         <div className="col">
                             <Badge
-                                firstName="Alejandro" 
-                                lastName="Salazar" 
-                                jobTitle="Frontend Engineer in Training"       
-                                twitter="dbriceno10" 
+                                firstName={this.state.form.firstName} 
+                                lastName={this.state.form.lastName} 
+                                jobTitle={this.state.form.jobTitle}       
+                                twitter={this.state.form.twitter}
+                                email={this.state.form.email}
                                 avatarUrl = "https://media-exp1.licdn.com/dms/image/C5603AQFILCh7FRKErA/profile-displayphoto-shrink_200_200/0/1617145732921?e=1626307200&v=beta&t=lkVAqzsfDRPW5nmUDPnXEVA38bpJYTI6R8KKdqWlvxs" />
                         </div>
                         {/*Vamos a agregar otra columna que va a contener nuesto formulario */}
                         <div className="col-6">
-                            <BadgeForm />
+                        {/**Ahora esta información del evento se la vamos a pasar a BadgeForm como un prop, para que el formulario tenga acceso al evento y acceda a la inforación.
+                        Hay que pasar los varlores del formulario desde BadgeNew hasta BadgeForm */}
+                            <BadgeForm 
+                                onChange={this.handleChange}
+                                formValues={this.state.form}
+                            />
                         </div>
                     </div>
                 </div>
